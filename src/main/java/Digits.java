4f0950d61422e5558a21fa3fa54a1c5b62c448c3
PrestonChengAPCSA <143027975+PrestonChengAPCSA@users.noreply.github.com>
2024-04-15 13:09:56 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -6,14 +6,32 @@ public class Digits
 	private ArrayList<Integer> digitList;
 
 	public Digits(int num)
-	{ /* to be implemented in part (a) */ 
-	    
+	{
+		digitList = new ArrayList<Integer>();
+		int n = num;
+		if(num == 0){
+			digistList.add(0);
+		}
+		int n = num;
+		while(n > 0){
+			digistList.add(0,n%10);
+			n = n/10;
+		}
+		
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-		
+		int value = false;
+		for(int i = 0; i < digitsList.length-1; i++){
+			if(digitsList.get(i) < digits.get(i+1)){
+				value = true;
+			} else {
+				value = false;
+			}
+		}	
 
+		return value;
 	}
 	
 	public String toString()
